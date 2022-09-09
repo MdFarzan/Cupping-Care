54f9a844d220ae3082f2d5dcbd30102701bb8f88
MdFarzan <mdfarzan776@gmail.com>
2022-09-09 09:37:22 +0530

    testimonial slider added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,45 @@
 */
 
 console.info("main script loaded.");
+// slider js starts
+const slides = document.querySelectorAll('.testimonial .slide');
+const timeLine = document.querySelectorAll('.testimonial .time-line li');
+const slidesWrap = document.querySelector('.testimonial .slides-wrap');
+
+var temp = Array.from(slides);
+
+var slideHeights = temp.map((slide) => {
+    return slide.offsetHeight;
+});
+
+
+
+var slideMaxHeight = Math.max.apply(null, slideHeights);
+slidesWrap.style.height = slideMaxHeight + 'px';
+
+var currentSlide = 0;
+
+
+slides.forEach((slide, index) => {
+    slide.style.transform = `translateX(${100 * index}%)`;
+    slide.children[0].style.height = slideMaxHeight+"px";
+});
+
+setInterval(() => {
+    next();
+}, 4000);
+
+function next() {
+    currentSlide++;
+
+    currentSlide = currentSlide == slides.length ? 0 : currentSlide;
+    slides.forEach((slide, index) => {
+        slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
+    });
+
+       
+
+}
+
+console.log(timeLine);
+// slider js ends
